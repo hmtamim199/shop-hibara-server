84c266cd51ae65a9b6805aeccd173e4114b05aa1
hmtamim199 <hmtamim8906@gmail.com>
2025-02-27 20:48:31 +0600

    new code written

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const express = require("express");
+// const express = require("express");
+// const app = express();
+// const cors = require('cors');
+// const port = process.env.PORT || 5000;
+
+// app.use(express.json());
+// app.use(cors())
+
+
+// app.get('/', (req, res) => {
+//   res.send('shop hibara server ')
+// })
+
+
+// app.listen(port, () => {
+//   console.log(`shop hibara server${port}`)
+// })
+
+const express = require('express');
 const app = express();
-const cors = require(cors.json())
+const cors = require('cors');
+
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use()
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('hibara shop server in running ')
+});
 
 
-app.get( '/', (req, res) => {
-  res.send('shop hibara server ')
-})
-
-app.listen( )
+app.listen(port, () => {
+  console.log(`hibara shop is running on ${port}`)
+});
